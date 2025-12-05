51169547095cb3c124fbbfa1621e4aed78e2b078
FernanMoreno <fernandojosemolinamore@gmail.com>
2025-12-05 01:09:50 +0100

    Feat: Agregando modelos de datos para las tablas en la bd

---
+++ models/projects.py
@@ -0,0 +1,43 @@
+import uuid
+from datetime import datetime
+from typing import List, Optional
+
+from sqlalchemy import (
+    String,
+    Text,
+    DateTime
+)
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+
+    description: Mapped[Optional[str]] = mapped_column(Text())
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        default=datetime.utcnow
+    )
+
+    runs: Mapped[List["Run"]] = relationship(
+        back_populates="project",
+        cascade="all, delete-orphan"
+    )
+
+    prompts: Mapped[List["Prompt"]] = relationship(
+        back_populates="project",
+        cascade="all, delete-orphan"
+    )
